e87f33b79d4e444f6a20704d806c2bad18008ff6
renzon <renzon@gmail.com>
2019-08-07 22:53:44 -0300

    Max resolvido na mão, passada tarefa de media para próxima semana

--- ad1/q1_leitor_media_maximo.py
+++ ad1/q1_leitor_media_maximo.py
@@ -1,12 +1,12 @@
-numeros = []   # numeros = [ 4.0, 5.0 ]
+numeros = [4.0, 5.0, 3.0]  # numeros = [ 4.0, 5.0 ]
 
-while True:
-    numero = input('Digite um número ou aperte enter para finalizar: ')  # numero = ''
-    if numero == '':
-        break
-    else:
-        numero = float(numero)
-        numeros.append(numero)
+# while True:
+#     numero = input('Digite um número ou aperte enter para finalizar: ')
+#     if numero == '':
+#         break
+#     else:
+#         numero = float(numero)
+#         numeros.append(numero)
 
 quantidade_de_numeros = len(numeros)  # quantidade_de_numeros = 2
 print(f'Quantidade de numeros inseridos: {quantidade_de_numeros}')
@@ -14,7 +14,10 @@ print(f'Quantidade de numeros inseridos: {quantidade_de_numeros}')
 if quantidade_de_numeros == 0:
     print('Não é possível calcular média e máximo pois não foi inserido nenhum valor')
 else:
-    maximo = max(numeros)  # maximo = 5.0
-    print(f'Valor máximo inserido foi {maximo}')
+    candidato_a_maximo = numeros[0]  # candidato_a_maximo = 2
+    for n in numeros:  # n = 1
+        if candidato_a_maximo < n:   # [1, 2, 1]
+            candidato_a_maximo = n
+    print(f'Valor máximo inserido foi {candidato_a_maximo}')
     media = sum(numeros) / quantidade_de_numeros  # 9 / 2 = 4.5
     print(f'A média dos valores inserido é {media}')
