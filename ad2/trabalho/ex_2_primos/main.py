a7c79d34668a94fc7fb3b8e28f0e76d2b1c67588
renzon <renzon@gmail.com>
2019-10-07 21:46:54 -0300

    Altarando nomes de arquivos de entrada e saida do ex 2

--- ad2/trabalho/ex_2_primos/main.py
+++ ad2/trabalho/ex_2_primos/main.py
@@ -2,9 +2,10 @@ def eh_primo(n: int) -> bool:
     return True
 
 
-# nome_do_arquivo = input('Digite o nome do entrada de entrada: ')
-arquivo_entrada = open('entrada.txt', 'r', encoding='utf8')
-arquivo_saida = open('saida.txt', 'w', encoding='utf8')
+nome_do_arquivo_de_entrada = input('Digite o nome do entrada de entrada: ')
+nome_do_arquivo_de_saida = input('Digite o nome do entrada de saída: ')
+arquivo_entrada = open(nome_do_arquivo_de_entrada, 'r', encoding='utf8')
+arquivo_saida = open(nome_do_arquivo_de_saida, 'w', encoding='utf8')
 with arquivo_entrada as entrada, arquivo_saida as saida:
     for linha in entrada:
         linha = linha.strip()
@@ -16,5 +17,3 @@ with arquivo_entrada as entrada, arquivo_saida as saida:
         linha_a_ser_escrita=' '.join(numeros_primos)
         saida.write(f'{linha_a_ser_escrita}\n')
 
-# print(eh_primo('ama'))
-# print(eh_primo('socorrammesubinoonibusemmarroco'))
